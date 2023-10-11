7ed003c917ac68998c1c7bb9b4c7c83f3246c62d
mingfeiq <mingfeiq@amazon.com>
2023-10-11 14:39:51 +0000

    new projen

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # csdc-cdk-constructs
+
+
+## Acknowledgements
+
+This project utilizes [projen](https://github.com/projen/projen) (_star it on GitHub_)
+and was created by following [this guide by hayao-k](https://dev.to/aws-builders/a-beginner-s-guide-to-create-aws-cdk-construct-library-with-projen-5eh4)
+(_like it on Dev.to_).
--- source/examples/stack-workflow/lib/stack-workflow-stack.ts
+++ source/examples/stack-workflow/lib/stack-workflow-stack.ts
@@ -1,11 +1,11 @@
+import { CSDCStackWorkflow } from '../../../patterns/@csdc-solutions-constructs/stack-workflow/lib/index';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CSDCStackWorkflow } from '../../../patterns/@csdc-solutions-constructs/csdc-stack-workflow/lib/index';
 
 export class StackWorkflowStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CSDCStackWorkflow(this, 'CSDCStackWorkflow', {});
+    new CSDCStackWorkflow(this, 'Workflow', {});
   }
 }
